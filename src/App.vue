e910aa18eac1b86b151f98751e97dc4e29671de7
Tal <talrasha007@gmail.com>
2023-01-01 12:42:39 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,10 @@ async function connect() {
 </script>
 
 <template>
-<div>
+<div class="panel">
+  <div class="left">
+    <button>asdf</button>
+  </div>
   <div class="cennter_console">
     <div v-if="!error" class="radio_info">
       <div class="freq">{{ (frequency / 1e6).toFixed(1) }}</div>
@@ -27,9 +30,14 @@ async function connect() {
       </div>
     </div>
   </div>
+  <div class="right">
+    <button>asdf</button>
+  </div>
+  <div class="bottom">
+    <button @click="connect" v-if="!device">连接</button>
+    <button @click="disconnect" v-if="device">断开</button>
+  </div>
 </div>
-<button @click="connect" v-if="!device">连接</button>
-<button @click="disconnect" v-if="device">断开</button>
 
 <p v-if="error">
   <pre v-if="!error.stack">{{ error }}</pre>
@@ -38,45 +46,58 @@ async function connect() {
 </template>
 
 <style scoped lang="scss">
-.cennter_console {
-  color: #bdfcdf;
-  background-color: #364334;
-  min-width: 220px;
-  height: 80px;
+.panel {
+  border-radius: 5px;
+  box-shadow: 1px 1px 3px 3px #888;
+  padding: 10px;
+  display: flex;
+  background-color: #dddddd;
+  flex-wrap: wrap;
 
-  .radio_info {
-    position: relative;
-    .freq {
-      line-height: 80px;
-      font-size: 60px;
-      font-weight: bold;
-    }
+  .bottom {
+    width: 100%;
+  }
+  
+  .cennter_console {
+    color: #bdfcdf;
+    background-color: #364334;
+    flex-grow: 2;
+    // min-width: 220px;
+    height: 80px;
 
-    .mode {
-      position: absolute;
-      right: 10px;
-      bottom: 10px;
-      font-size: 15px;
-      font-weight: bold;
-    }
+    .radio_info {
+      position: relative;
+      .freq {
+        line-height: 80px;
+        font-size: 60px;
+        font-weight: bold;
+      }
 
-    .signal_level {
-      position: absolute;
-      right: 16px;
-      bottom: 35px;
-      height: 30px;
-      width: 10px;
-      background-color: #3d7457;
-
-      .bar {
-        width: 100%;
-        background-color: #bdfcdf;
+      .mode {
         position: absolute;
-        left: 0;
-        bottom: 0;
+        right: 10px;
+        bottom: 10px;
+        font-size: 15px;
+        font-weight: bold;
+      }
+
+      .signal_level {
+        position: absolute;
+        right: 16px;
+        bottom: 35px;
+        height: 30px;
+        width: 10px;
+        background-color: #3d7457;
+
+        .bar {
+          width: 100%;
+          background-color: #bdfcdf;
+          position: absolute;
+          left: 0;
+          bottom: 0;
+        }
       }
     }
-  }
-
-}
+  } // centor_console
+} // panel
 </style>
